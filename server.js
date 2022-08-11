9f1f4c6952caa2a934b736222c2d3d3274ac65f2
Aysun <aysunkapusuz@gmail.com>
2022-08-11 07:19:15 -0600

    ejs files created

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const { response } = require("express");
 const express = require("express")
 const app = express()
 const PORT =8500;
 const mongoose = require("mongoose")
 require('dotenv').config()
-const GroceryList = require('./models/Grocery')
+const Grocery = require('./models/grocery')
 
 
 app.set("view engine", "ejs")
@@ -17,14 +18,85 @@ mongoose.connect(process.env.DB_STRING,
 
 app.get('/', async (req,res) => {
     try{
-        Grocery.find({}, (err, groceries) => {
-        res.render('index.ejs', {grocery: groceries})
-    })
-    }catch(err){
-
+        Grocery.find({}, (err, groceries) =>{
+            res.render('index.ejs', {
+                grocery: groceries
+            })
+        })
+    } catch (error){
+        res.status(500).send({message: error.message})
     }
-}
-)
+})
+
+app.post('/', async(req,res) => {
+    const grocery = new Grocery(
+        {
+            category: req.body.category,
+            item: req.body.item
+        }
+    )
+    try {
+        await grocery.save()
+        console.log(grocery)
+        res.redirect("/")
+    }catch (err){
+        if (err) return res.status(500).send(err)
+        res.redirect('/')
+    }
+})
+
+
+app
+   .route("/edit/:id").get((req,res) => {
+    const id = req.params.id
+    Grocery.find({}, (err, groceries) => {
+        res.render('edit.ejs',{
+            grocery:groceries, idData: id
+        })
+    })
+    .post((req,res) =>{
+        const id = req.params.id
+        Grocery.findByIdAndUpdate(
+            id,
+            {
+              category: req.body.category,
+              item: req.body.item
+            },
+            err => {
+                if (err) return res.status(500).send(err)
+                res.redirect('/')
+            }
+        )
+    })
+})
+function getRequestHandler(req, res) {
+    const id = req.params.id;
+    Grocery.find({}, (err, groceries) => {
+      res.render("edit.ejs", {
+        grocery: groceries,
+        idData: id,
+      });
+    });
+  }
+  
+  function postRequestHandler(req, res) {
+    const id = req.params.id;
+    Grocery.findByIdAndUpdate(
+      id,
+      {
+        category: req.body.category,
+        item: req.body.item,
+      },
+      (err) => {
+        if (err) return res.status(500).send(err);
+        res.redirect("/");
+      }
+    );
+  }
+  
+  app.route("/edit/:id").get(getRequestHandler).post(postRequestHandler);
+    
+
 
 app.listen(PORT, () =>
     console.log(`Server is running on port ${PORT}`)
